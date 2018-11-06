a41e6a1b0b30faafc4adb925028eafd6f56388e4
Phillip Grønfeldt <PhillipG@Phillips-MacBook-Air.local>
2018-11-06 16:45:54 +0100

    Caching videre

--- src/main/java/cache/UserCache.java
+++ src/main/java/cache/UserCache.java
@@ -1,6 +1,7 @@
 package cache;
 
 import controllers.OrderController;
+import controllers.UserController;
 import model.User;
 import utils.Config;
 
@@ -8,4 +9,28 @@ import java.util.ArrayList;
 
 //TODO: Build this cache and use it.
 public class UserCache {
+
+    private ArrayList<User> users;
+
+    private long ttl;
+
+    private long created;
+
+    public UserCache() {
+        this.ttl = Config.getUserTtl();
+    }
+
+    public ArrayList<User> getUsers(Boolean forceUpdate) {
+       if (forceUpdate
+           || ((this.created + this.ttl) <= (System.currentTimeMillis() / 1000L))
+           || this.users.isEmpty()) {
+
+           ArrayList<User> users = UserController.getUsers();
+
+           this.users = users;
+           this.created = System.currentTimeMillis() / 1000L;
+       }
+       return this.users;
+    }
+}
 
